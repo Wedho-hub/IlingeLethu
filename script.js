c7b9f420b0804d071fd7e69ee2670fc522795d83
wedho <wellingtond99@gmail.com>
2024-01-08 23:01:15 +0200

    second commit

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 
-const hamburgerBtn = document.getElementById('humburger-btn');
-const menu = document.querySelector('.menu');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerBtn = document.getElementById('humburger-btn');
+  const menu = document.querySelector('.menu');
 
-hamburgerBtn.addEventListener('click', () => {
-  menu.classList.toggle('show-menu');
-  hamburgerBtn.classList.toggle('open');
-});
-
-const form = document.getElementById('myForm');
-
-form.addEventListener('submit', async function(event) {
-  event.preventDefault(); // Prevent form submission
-
-  // Collect form data
-  const formData = new FormData(form);
-  
-  // Convert form data to an object
-  const data = {};
-  formData.forEach((value, key) => {
-    data[key] = value;
+  hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.classList.toggle('open');
+    menu.classList.toggle('show-menu');
   });
-
-  try {
-    // Send the form data to the server (example using fetch)
-    const response = await fetch('/submit-form', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-
-    if (response.ok) {
-      // Form data successfully sent
-      console.log('Form data submitted to server.');
-      // Optionally, perform actions based on the response
-    } else {
-      // Handle errors if the submission fails
-      console.error('Failed to submit form data.');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
 });
+
+
+// const form = document.getElementById('myForm');
+
+// form.addEventListener('submit', async function(event) {
+//   event.preventDefault(); // Prevent form submission
+
+//   // Collect form data
+//   const formData = new FormData(form);
+  
+//   // Convert form data to an object
+//   const data = {};
+//   formData.forEach((value, key) => {
+//     data[key] = value;
+//   });
+
+//   try {
+//     // Send the form data to the server (example using fetch)
+//     const response = await fetch('/submit-form', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(data)
+//     });
+
+//     if (response.ok) {
+//       // Form data successfully sent
+//       console.log('Form data submitted to server.');
+//       // Optionally, perform actions based on the response
+//     } else {
+//       // Handle errors if the submission fails
+//       console.error('Failed to submit form data.');
+//     }
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// });
